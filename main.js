49921a22cfdd7e9cd1b44eb5032e63089279a8d9
Fernanda Yukimi <ferohnishi4@gmail.com>
2024-11-14 22:33:30 -0400

    iniciando projeto

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
-var ola;
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 
-var george = "aasdsdasdas";
+const client = new Client();
+
+client.once('ready', () => {
+    console.log('Bot está pronto!');
+});
+
+// Gerar QR Code
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+
+
+client.on('message_create', message => {	
+const regex = /^\/tarefas\s+"(.+?)"\s+(\d{2}\/\d{2}\/\d{4})$/;
+const match = message.body.match(regex) // Verifica se bate com a mensagem
+
+if(match){
+	var tarefa = match[1]; //Captura a tarefa que está em ()
+	var data = match[2] // Captura a data que está em ()
+
+	client.sendMessage(message.from, `${tarefa} cadastrada para o dia ${data}`)
+}
+
+	if (message.body === '!ping') {
+		client.sendMessage(message.from, 'pong');
+	}
+});
+// Start your client
+client.initialize();
