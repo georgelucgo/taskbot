3e940ce44a14122141d80d4ee9c9c102e453b0d7
georgelucgo <georgelucgo@gmail.com>
2024-11-18 10:57:38 -0400

    adicionando banco de dados

---
+++ database.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgfF640KAC5D31E03eKbG74Cj_8Nscg0I",
+  authDomain: "tarefex-afd72.firebaseapp.com",
+  projectId: "tarefex-afd72",
+  storageBucket: "tarefex-afd72.firebasestorage.app",
+  messagingSenderId: "786446728943",
+  appId: "1:786446728943:web:bf96fc554d22975249651a",
+  measurementId: "G-9CFVQDMJWX"
+};
+
+const app = initializeApp(firebaseConfig);
+
+
