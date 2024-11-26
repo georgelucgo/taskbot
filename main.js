8c6e6018c3df2b8db3e4180244714207e877d30f
georgelucgo <georgelucgo@gmail.com>
2024-11-26 11:06:28 -0400

    adicionando Editar

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 const { Client, NoAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-const { collection, addDoc, doc, getDocs, query, orderBy, limit, deleteDoc, where } = require("firebase/firestore");
+const { collection, addDoc, doc, getDocs, query, orderBy, limit, deleteDoc, where, updateDoc } = require("firebase/firestore");
 const db = require("./database"); // Arquivo onde Firestore está configurado
 
 const client = new Client({
-	authStrategy: new NoAuth() // Para não precisar ficar conectando toda hora
 });
 
 client.once('ready', () => {
@@ -21,6 +20,8 @@ const tarefasCollection = collection(db, "tarefas");
 
 
 client.on('message_create', async (message) => {
+
+	// Método Cadastrar 
     const regex = /^\/tarefas\s+"(.+?)"\s+(\d{2})\/(\d{2})\/(\d{4})$/;
     const match = message.body.match(regex); // Verifica se bate com a mensagem
 
@@ -55,12 +56,14 @@ client.on('message_create', async (message) => {
 					id: id,
 					tarefa: tarefa,
 					data: data.toLocaleDateString("pt-BR"),
-					concluido: false
+					concluido: false,
+					created: new Date()
 				});
 		
 				console.log("Documento adicionado com ID:", id);
 				// Envia a mensagem de confirmação
 				client.sendMessage(message.from, `${tarefa} cadastrada para o dia ${data.toLocaleDateString("pt-BR")}`);
+				listarTarefas();
 			} catch (error) {
 				console.error("Erro ao adicionar documento:", error);
 				client.sendMessage(message.from, "Houve um erro ao cadastrar a tarefa. Tente novamente.");
@@ -69,11 +72,14 @@ client.on('message_create', async (message) => {
 
 		cadastrarTarefa(novoId, tarefaFormatada, data)
     }
-	
-	async function listarTarefas(){
+	// Fim do Método Cadastrar
 
+
+	// Método Listar 
+	async function listarTarefas(){
 		try{
-			const querySnapshot = await getDocs(tarefasCollection);
+			const q = query(tarefasCollection, orderBy("id", "asc"), )
+			const querySnapshot = await getDocs(q);
 			if(querySnapshot.empty){
 				client.sendMessage(message.from, "Nenhuma tarefa encontrada.");
 				return
@@ -94,9 +100,51 @@ client.on('message_create', async (message) => {
 		
 	}
 
+	if (message.body === '/listar') {
+		listarTarefas();
+	}
+
+	// Fim do Método Listar
+
+
+	// Método Editar
+	async function editarTarefas(id, tarefa, data){
+			const q = query(tarefasCollection, where("id", "==", parseInt(id))); // Busca o campo 'id'
+			const querySnapshot = await getDocs(q);
+			if (!querySnapshot.empty) {
+				const docRef = doc(db, "tarefas", querySnapshot.docs[0].id);
+				await updateDoc(docRef, {
+					id: parseInt(id),
+					tarefa: tarefa,
+					data: data.toLocaleDateString("pt-BR")
+				})
+				client.sendMessage(message.from, `Tarefa ${id} atualizada com sucesso!`);
+			}else {
+				// Caso o documento não seja encontrado
+				client.sendMessage(message.from, `Tarefa com ID ${id} não encontrada.`);
+			}
+		
+	}
+
+	const editarRegex = /^\/editar\s+(\d+)\s+"(.+?)"\s+(\d{2})\/(\d{2})\/(\d{4})$/
+	const editarMatch = message.body.match(editarRegex)
+	//Se a mensagem for igual
+	if (editarMatch) {
+		const idEditar = editarMatch[1];
+		const tarefaEditar = editarMatch[2];
+		let diaEdit = parseInt(editarMatch[3]);
+        let mesEdit = parseInt(editarMatch[4]);
+        let anoEdit = parseInt(editarMatch[5]);
+		let horaEdit = 23;
+		let minutosEdit = 59;
+        const data = new Date(anoEdit, mesEdit - 1, diaEdit, horaEdit, minutosEdit);
+		editarTarefas(idEditar, tarefaEditar, data);
+	}
+	// Fim do Método Editar
 
 
 
+	// Método de Deletar
 	async function deletarTarefas(idTarefa){
 		try{
 		const q = query(tarefasCollection, where("id", "==", parseInt(idTarefa))); // Busca o campo 'id'
@@ -107,6 +155,7 @@ client.on('message_create', async (message) => {
             await deleteDoc(docRef);
             console.log(`Tarefa com ID ${idTarefa} deletada.`);
             client.sendMessage(message.from, "Tarefa deletada com sucesso");
+			listarTarefas();
         } else {
             // Caso o documento não seja encontrado
             client.sendMessage(message.from, `Tarefa com ID ${idTarefa} não encontrada.`);
@@ -118,18 +167,47 @@ client.on('message_create', async (message) => {
 		
 	}
 
+
+	//Capturando mensagem
 	const deletarRegex = /^\/deletar\s+(\d+)$/
 	const deletarMatch = message.body.match(deletarRegex)
+	//Se a mensagem for igual
 	if (deletarMatch) {
 		const idTarefa = deletarMatch[1];
 		deletarTarefas(idTarefa);
 	}
-	
 
-	if (message.body === '/listar') {
-		listarTarefas();
-	}
+	// Fim do Método deletar
 
+
+
+
+	// function avisoTarefa(id){
+	// 	// Faltando 1 dia para tarefa ser finalizada vai chamar essa função
+	// 	// Criar um meio de avisar
+	// 	client.sendMessage(message.from, `${tarefa} vai se encerrar daqui 1 dia!!`)
+	// }
+
+	// //Fora do função ela tem que ser chamada
+	// //Algo tipo assim
+
+	// if(dataFaltando === 1){  
+	// 	avisoTarefa(tarefa.id);
+	// }
+
+
+	// function tarefaFinalizada(){
+	// 	//Temos cadastrada a conclusão, basicamente vamos buscar e deletar
+	// 	//Algo tipo assim:
+
+	// 	//if(Date.now() == tarefa.data){
+	// 		//tarefas.concluido = true;
+	// 		client.sendMessage(message.from, `${tarefas.tarefa} encerrou o seu prazo!`)
+	// 	}
+
+	// }
+
+	// tarefaFinalizada()
 
 
     if (message.body === '!ping') {
